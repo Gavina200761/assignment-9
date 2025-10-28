ea55504898fb41ce933a787cd58f8067da3b7e38
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-10-28 07:52:16 -0400

    The API successfully renders my GIFs and they appear after the click of a button.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,19 @@ console.log("script.js loaded");
 
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=thRm01WDPpqDYkKeymvtkgCMRkePkAx3&q=giphy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+let images = [];
+let gifContainer = document.getElementById("gif-container");
+//let totalURLS = data.data.length;
+let button = document.getElementById("gif-btn");
+
 async function getGifs() {
     const response = await fetch(endpoint);
     const data = await response.json();
-    let totalURLS = data.data.length;
-    let images = [];
 
-    for (let i = 0; i < totalURLS; i++) {
-        const loadImage = data.data[i]["url"];
+    images = [];
+
+    for (let i = 0; i < data.data.length; i++) {
+        const loadImage = data.data[i].images.original.url;
 
         images.push(loadImage);
     };
@@ -17,4 +22,13 @@ async function getGifs() {
     console.log("images: ", images);
 };
 
-getGifs()
+getGifs();
+
+button.addEventListener("click", function () {
+    gifContainer.innerHTML = "";
+
+    for(let i = 0; i < images.length; i++) {
+        gifContainer.innerHTML += `<img src=${images[i]} class = "col-3 mb-3">`;
+    }
+});
+
