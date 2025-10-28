ca37b8b75cf7b9a9e73ad0bb89938cb89fc6e35d
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-10-28 19:27:31 -0400

    The code was cleaned up and I used bootstrap to add some visual effects to the site.

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 console.log("script.js loaded");
-
+//endpoint-source of the GIFs being rendered.
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=thRm01WDPpqDYkKeymvtkgCMRkePkAx3&q=giphy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
-let images = [];
+//Defining variables that will hold the gif container and button.
 let gifContainer = document.getElementById("gif-container");
-//let totalURLS = data.data.length;
 let button = document.getElementById("gif-btn");
 
+//Async function that 
 async function getGifs() {
     const response = await fetch(endpoint);
     const data = await response.json();
+    
+    images = []; //Ensures that the images array is empty.
 
-    images = [];
-
+    //Loop that adds an image element for each image URL to the gifContainer.
     for (let i = 0; i < data.data.length; i++) {
         const loadImage = data.data[i].images.original.url;
 
         images.push(loadImage);
     };
-
-    console.log("images: ", images);
 };
 
+//Calling the getGifs() function to gather data before displaying it.
 getGifs();
 
+//Loops through all URLs in images, adding <img> elements for each one.
 button.addEventListener("click", function () {
     gifContainer.innerHTML = "";
 
