5ca21dc425444f00fd541f6231b037b12cda34ab
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-10-28 04:19:36 -0400

    I created a variable that holds the Request URL.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=thRm01WDPpqDYkKeymvtkgCMRkePkAx3&q=giphy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
