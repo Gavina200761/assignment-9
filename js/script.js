28bf64191b684df48d74dddd2f9420acfc3e41c1
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-10-28 06:00:32 -0400

    I loaded all of the URLs from the endpoint to an array called "images".

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 console.log("script.js loaded");
 
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=thRm01WDPpqDYkKeymvtkgCMRkePkAx3&q=giphy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+async function getGifs() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    let totalURLS = data.data.length;
+    let images = [];
+
+    for (let i = 0; i < totalURLS; i++) {
+        const loadImage = data.data[i]["url"];
+
+        images.push(loadImage);
+    };
+
+    console.log("images: ", images);
+};
+
+getGifs()
