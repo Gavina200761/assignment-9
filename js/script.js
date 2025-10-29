5774cea7ee01390ac52313dc50eac81763119927
Gavina200761 <gavina200761@students.reinhardt.edu>
2025-10-29 19:25:21 -0400

    Final Changes were made to correct misunderstandings.

--- js/script.js
+++ js/script.js
@@ -3,17 +3,18 @@ console.log("script.js loaded");
 let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=thRm01WDPpqDYkKeymvtkgCMRkePkAx3&q=giphy&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
 //Defining variables that will hold the gif container and button.
-let gifContainer = document.getElementById("gif-container");
-let button = document.getElementById("gif-btn");
+let gifContainer = document.querySelector("#gif-container");
+let button = document.querySelector("#gif-btn");
+let images = [];
 
-//Async function that 
+//Async function that pulls from the endpoint site and waits for a response, then converts it to a readable file and stores all GIF URLs in the images array.
 async function getGifs() {
     const response = await fetch(endpoint);
     const data = await response.json();
     
     images = []; //Ensures that the images array is empty.
 
-    //Loop that adds an image element for each image URL to the gifContainer.
+    //Loop that adds the URLs to images array
     for (let i = 0; i < data.data.length; i++) {
         const loadImage = data.data[i].images.original.url;
 
